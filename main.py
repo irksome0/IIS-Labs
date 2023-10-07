fe785f93f2bbb2c5c5648f3815201952a34ee1db
irksome0 <125121047+irksome0@users.noreply.github.com>
2023-10-07 10:44:06 +0300

    Made a data reading and filtering

---
+++ main.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_data():
+    data = pd.read_csv("police_project.csv", low_memory=False)
+    data = pd.DataFrame(data[0:100000])
+    return data
+
+
+def filter_data(data_frame):
+    filtered_data = data_frame[['date', 'subject_sex', 'search_conducted', 'search_basis', 'raw_search_vehicle_description', 'reason_for_stop']]
+    return filtered_data
+
+
+def moving_violation(data_frame):
+    current_data = data_frame[lambda x: x['reason_for_stop'] == 'Moving Violation']
+
+    males = len(data_frame[lambda x: x['subject_sex'] == 'male'])
+    females = len(data_frame[lambda x: x['subject_sex'] == 'female'])
+
+    current_data["subject_sex"].value_counts().plot(kind="bar", fontsize=10)
+    plt.title("Moving violation statistics by gender")
+    plt.show()
+    if males > females:
+        return males
+    else:
+        return females
+
+
+def search_quantity(data_frame):
+    current_data = data_frame[lambda x: x['search_conducted'] == True]
+
+    current_data['subject_sex'].value_counts().plot(kind="bar", fontsize=10)
+    plt.title("Search conduction statistics by gender")
+    plt.show()
+
+
+def search_type(data_frame):
+    current_data = data_frame[lambda x: x['search_conducted'] == True]
+    current_data['raw_search_vehicle_description'].value_counts().plot(kind="bar", fontsize=10)
+    plt.title("Search types")
+    plt.show()
+
+
+data = get_data()
+print(filter_data(data))
+print(moving_violation(data))
+search_quantity(data)
+search_type(data)
